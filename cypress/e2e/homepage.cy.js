5bd612c073ec8969dd422a4c6b54faf41ed42409
Alexander Giannini <alexgiannini@Alexanders-MBP.lan>
2024-05-14 12:36:29 -0700

    added more homepage tests

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -16,17 +16,48 @@ describe('Nike homepage tests', () => {
   });
 
   it('should select favorites as a logged out user', () => {
-    cy.titleAttr('Favorites').scrollIntoView().click();
+    cy.ariaLabel('Favorites').eq(1).click();
     cy.ariaLabel('Enter your email to join us or sign in.').should('be.visible');
+  });
+
+  it('should select nav cart as a user who does not have any items', () => {
+    cy.get('#nav-cart').scrollIntoView().click();
+    cy.get('p.css-pm6c6.e14faaxk0').contains('There are no items in your bag.').should('be.visible');
   });
 
   it('should select Help within the footer', () => {
     cy.ariaLabel('main-footer, main-footer, Help').scrollIntoView().click();
     cy.url().should('include', '/help');
-  })
+  });
 
   it('should select Shop Mens New Arrivals', () => {
     cy.ariaLabel("Shop Men's New Arrivals ").scrollIntoView().click();
     cy.get('.wall-header__title').contains("Men's New Releases").should('be.visible');
-  })
-})
+  });
+
+  it('should select Converse symbol', () => {
+    cy.ariaLabel('Converse').eq(0).click();
+    cy.get('.wall-header__title').contains("Converse Shoes").should('be.visible');
+  });
+
+  it('should select Air Max component', () => {
+    cy.ariaLabel('Air Max').eq(0).scrollIntoView().click();
+    cy.get('.wall-header__title').contains("Air Max Shoes").should('be.visible');
+  });
+
+  it('should select Jordan New Arrivals Card', () => {
+    cy.ariaLabel('New Arrivals Jordan Apparel').eq(1).scrollIntoView().click();
+    cy.get('.wall-header__title').contains("Jordan New Releases").should('be.visible');
+  });
+
+  it('should select Help within navigation', () => {
+    cy.ariaLabel('Help').eq(0).click();
+    cy.get('.pre-help-dropdown-menu-item').contains('Order Status').click();
+    cy.get('.headline-4').contains('View or Manage Your Order').should('be.visible');
+  });
+
+  it('should select Nike Member Card at bottom of home page', () => {
+    cy.ariaLabel('Member Product Your Exclusive Access').scrollIntoView().click();
+    cy.get('div._1ZsY6zh2.headline-3').contains('Nike Membership').should('be.visible');
+  });
+});
